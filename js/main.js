d784d81921980417792f7f0c01ee6ff539ab1372
unknown <kd241286zjv@gmail.com>
2018-09-09 19:26:48 +0300

    start

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ $(document).ready(function(){
 
   var mainPageSwiper = new Swiper('.mainPage-swiper-container', {
     pagination: {
-      el: '.swiper-pagination',
+      el: '.mainPage-swiper-pagination',
       clickable: true,
     },
   });
@@ -42,6 +42,65 @@ $(document).ready(function(){
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
+    },
+  });
+
+  $.scrollify({
+    section : "section",
+    interstitialSection : "",
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset : 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll:true,
+    before:function(i,panels) {
+
+      var ref = panels[i].attr("data-section-name");
+
+      $(".scroll-pagination .active").removeClass("active");
+
+      $(".scroll-pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+
+      if(!$('.scroll-pagination li a[href="#01"]').hasClass('active')){
+        $('.scroll-pagination').addClass('darken');
+      }
+      else{
+        $('.scroll-pagination').removeClass('darken');
+      }
+    },
+    afterRender:function() {
+      var pagination = "<ul class=\"scroll-pagination\">";
+      var activeClass = "";
+      $("section").each(function(i) {
+        activeClass = "";
+        if(i===0) {
+          activeClass = "active";
+        }
+        pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+      });
+
+      pagination += "</ul>";
+
+      $(".mainPage").append(pagination);
+      /*
+
+      Tip: The two click events below are the same:
+
+      $(".pagination a").on("click",function() {
+        $.scrollify.move($(this).attr("href"));
+      });
+
+      */
+      $(".scroll-pagination a").on("click",$.scrollify.move);
+    },
+    after: function(){
+      if($('.scroll-pagination li a[href="#02"]').hasClass('active')){
+        $('.mainSection').addClass('animatedSection');
+      }
     },
   });
 });
